6bea5dd6e56dd11131db502586f3c00ffb62a097
corey-stidston <corey.stidston@outlook.com>
2026-01-31 08:59:39 +1100

    feat: create a new symptom

--- app/controllers/symptoms_controller.rb
+++ app/controllers/symptoms_controller.rb
@@ -2,4 +2,26 @@ class SymptomsController < ApplicationController
   def index
     @symptoms = Symptom.all
   end
+
+  def show
+    @symptom = Symptom.find(params[:id])
+  end
+
+  def new
+    @symptom = Symptom.new
+  end
+
+  def create
+    @symptom = Symptom.new(symptom_params)
+    if @symptom.save
+      redirect_to @symptom
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def symptom_params
+      params.expect(symptom: [ :name ])
+    end
 end
