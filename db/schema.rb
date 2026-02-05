234cc1d2cb67d47205530d6410435c5d6a725d92
corey-stidston <corey.stidston@outlook.com>
2026-02-05 20:28:32 +1100

    feat: add problems controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_01_080951) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_05_091917) do
+  create_table "problems", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "details"
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "symptoms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
