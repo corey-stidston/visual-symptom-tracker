8f49eb049b0e5136c3e67c28918746d0b0e357b4
corey-stidston <corey.stidston@outlook.com>
2026-02-06 13:58:39 +1100

    feat: update chart data generation and format observation date display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,9 @@ class HomeController < ApplicationController
   def index
     @problems = Problem.all
     @observations = Observation.all
+
+    @chart_data = @observations.map do |observation|
+      [ observation.observed.iso8601, observation.score ]
+    end
   end
 end
