34fd612ad8484aaab64a55409349185e7a0e21fb
corey-stidston <corey.stidston@outlook.com>
2026-02-06 13:47:23 +1100

    refactor: remove chart-related components and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :observations
   resources :problems
-  get "chart/index"
+
   get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
